44cbfbfdcfaba471e8336345583de7d913ed127d
jcampbell57 <jcampbell57@gmail.com>
2024-06-10 20:59:20 -0700

    update index file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import _ from 'lodash';
-import './style.css';
-import './reset.css';
-import Icon from './icon.png';
-import printMe from './print.js';
+import './stylesheets/reset.css'
+import './stylesheets/style.css'
+import initialize from './modules/pageLoader.js'
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-  element.appendChild(btn);
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
+initialize()
