08ba416e2b92894a88b9dfa8f5d4ed30a233a85d
jcampbell57 <jcampbell57@gmail.com>
2024-06-12 17:59:31 -0700

    configure jest to use mock file

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'node',
 }
