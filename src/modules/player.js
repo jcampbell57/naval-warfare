13b8be86ab7eb28e6bff329631368f8ad5a33204
jcampbell57 <jcampbell57@gmail.com>
2024-06-13 00:09:15 -0700

    update player class

--- src/modules/player.js
+++ src/modules/player.js
@@ -6,6 +6,22 @@ class Player {
     this.type = type
     this.gameboard = new Gameboard()
   }
+
+  makeRandomMove() {
+    while (true) {
+      const row = Math.floor(Math.random() * 10)
+      const col = Math.floor(Math.random() * 10)
+
+      if (
+        this.gameboard.tiles[row][col] !== 'hit' &&
+        this.gameboard.tiles[row][col] !== 'miss'
+      ) {
+        console.log(this.gameboard.tiles[row][col])
+        this.gameboard.receiveAttack([row, col])
+        return
+      }
+    }
+  }
 }
 
 export default Player
