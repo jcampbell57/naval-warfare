3bac7d307ff5cb4f7d39903f364ad46f9084f323
jcampbell57 <jcampbell57@gmail.com>
2024-06-19 00:06:14 -0700

    add alt values to icons

--- src/modules/icons.js
+++ src/modules/icons.js
@@ -4,6 +4,7 @@ import missIcon from '../assets/sea-water-svgrepo-com.svg'
 const createHitIcon = () => {
   const newHitIcon = document.createElement('img')
   newHitIcon.src = hitIcon
+  newHitIcon.alt = 'hit'
   newHitIcon.classList.add('boardIcon', 'hitIcon')
   return newHitIcon
 }
@@ -11,6 +12,7 @@ const createHitIcon = () => {
 const createMissIcon = () => {
   const newMissIcon = document.createElement('img')
   newMissIcon.src = missIcon
+  newMissIcon.alt = 'miss'
   newMissIcon.classList.add('boardIcon', 'missIcon')
   return newMissIcon
 }
